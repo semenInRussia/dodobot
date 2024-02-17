5deb4f8851165af11ecef5e00c773d8f591d93a1
semenInRussia <hrams205@gmail.com>
2024-02-17 23:02:39 +0300

    gamer: some stupid logs

--- gamer.py
+++ gamer.py
@@ -1,12 +1,27 @@
+_show = True
+if _show:
+    print("importing...")
+
 from screener import screen
 from tsrct import extract_table
 from worder import search
 
 
-def print_words() -> None:
-    tbl = extract_table(screen(), show=False)
+def print_words(show=False) -> None:
+    _maybe_print("screening...", show)
+    img = screen()
+
+    _maybe_print("extract text...", show)
+    tbl = extract_table(img, show=False)
     _print_table(tbl)
-    search(tbl, show=True)
+
+    _maybe_print("searching...", show)
+    search(tbl, show=show)
+
+
+def _maybe_print(s: str, show=False, *args, **kwargs):
+    if show:
+        print(s, *args, **kwargs)
 
 
 def _print_table(table: list[str]) -> None:
@@ -15,4 +30,4 @@ def _print_table(table: list[str]) -> None:
 
 
 if __name__ == "__main__":
-    print_words()
+    print_words(show=_show)
