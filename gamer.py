2c9c7889711ef35a0d9bd5ec4478f7acd4b690b1
semenInRussia <hrams205@gmail.com>
2024-02-17 19:57:48 +0300

    gamer: new print_table method

--- gamer.py
+++ gamer.py
@@ -1,15 +1,18 @@
-from pprint import pprint
-
 from screener import screen
 from tsrct import extract_table
 from worder import search
 
 
-def print_words():
-    img = screen()
-    tbl = extract_table(img, show=True)
-    pprint(tbl)
+def print_words() -> None:
+    tbl = extract_table(screen(), show=False)
+    _print_table(tbl)
     search(tbl, show=True)
 
 
-print_words()
+def _print_table(table: list[str]) -> None:
+    for row in table:
+        print(row)
+
+
+if __name__ == "__main__":
+    print_words()
