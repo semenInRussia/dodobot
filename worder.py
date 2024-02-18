ba338e7abbce79b2e35b8b4c6b5b62ec532beeaa
semenInRussia <hrams205@gmail.com>
2024-02-18 16:22:00 +0300

    worder: rebuild save word path way, save exact points every time

--- worder.py
+++ worder.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from enum import Enum
 
 n = 5
 MAX_WORD_LEN = 5
@@ -10,45 +9,36 @@ with open("dict.txt", "r") as f:
     words = set(map(str.strip, f))
 
 
-class Dir(Enum):
-    upl = 1
-    up = 2
-    upr = 3
-    r = 4
-    downr = 5
-    down = 6
-    downl = 7
-    l = 8
+Point = tuple[int, int]
 
-
-dirs = [
-    (Dir.upl, (-1, -1)),
-    (Dir.up, (-1, 0)),
-    (Dir.upr, (-1, +1)),
-    (Dir.r, (0, +1)),
-    (Dir.downr, (+1, +1)),
-    (Dir.down, (+1, 0)),
-    (Dir.downl, (+1, -1)),
-    (Dir.l, (0, -1)),
+_deltas: list[Point] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, +1),
+    (0, +1),
+    (+1, +1),
+    (+1, 0),
+    (+1, -1),
+    (0, -1),
 ]
 
 
 @dataclass
-class Path:
-    first: tuple[int, int]
-    rest: list[Dir]
+class WordPath:
+    first: Point
+    rest: list[Point]
 
-    def add(self, d: Dir):
-        return Path(first=self.first, rest=self.rest + [d])
+    def add(self, d: Point):
+        return WordPath(first=self.first, rest=self.rest + [d])
 
 
 _checked_words: set[str] = set()
 _used: list[list[bool]] = [[False for _ in range(n)] for _ in range(n)]
 _table: list[str] = []
-_paths: list[Path] = []
+_paths: list[WordPath] = []
 
 
-def search(table: list[str], show=False) -> list[Path]:
+def search(table: list[str], show=False) -> list[WordPath]:
     global _checked_words, _used, _table, _paths
 
     _checked_words = set()
@@ -57,7 +47,7 @@ def search(table: list[str], show=False) -> list[Path]:
 
     for i in range(n):
         for j in range(n):
-            path = Path(first=(i, j), rest=[])
+            path = WordPath(first=(i, j), rest=[])
             _fill_matrix(_used, False)
             _dfs(i, j, path=path, word="")
 
@@ -70,7 +60,7 @@ def search(table: list[str], show=False) -> list[Path]:
     return _paths
 
 
-def _dfs(i: int, j: int, path: Path, word: str):
+def _dfs(i: int, j: int, path: WordPath, word: str):
     global _checked_words, _used, _table, _paths
 
     if i < 0 or i >= n or j < 0 or j >= n:
@@ -89,8 +79,8 @@ def _dfs(i: int, j: int, path: Path, word: str):
 
     _used[i][j] = True
 
-    for d, (di, dj) in dirs:
-        _dfs(i + di, j + dj, path=path.add(d), word=word)
+    for di, dj in _deltas:
+        _dfs(i + di, j + dj, path=path.add((i + di, j + dj)), word=word)
 
     _used[i][j] = False
 
@@ -106,6 +96,14 @@ def _fill_matrix(m: list[list], val):
 
 
 if __name__ == "__main__":
-    tbl = ["бвгде", "ёзилм", "журка", "нопст", "фхцшщ"]
-    path = Path(first=(0, 1), rest=[])
-    search(tbl, show=True)
+    tbl = [
+        "бвгде",
+        "ёзилм",
+        "журка",
+        "нопст",
+        "фхцшщ",
+    ]
+    path = WordPath(first=(0, 1), rest=[])
+    paths = search(tbl, show=True)
+    for p in paths:
+        print(p)
